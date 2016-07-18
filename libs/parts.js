0c3c7f8d40e54df89e6626555e564e47e1225c2e
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-07-18 16:00:10 -0500

    add minification'

--- libs/parts.js
+++ libs/parts.js
@@ -49,3 +49,15 @@ exports.setupCSS = function(paths) {
     }
   };
 }
+
+exports.minify = function() {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  };
+}
