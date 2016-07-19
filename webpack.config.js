ed297baeefd1fa896aa3fe03b60001d3db4cba44
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-07-19 14:54:18 -0500

    add git ignore

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,8 @@ switch(process.env.npm_lifecycle_event) {
         devtool: 'source-map',
          output: {
           path: PATHS.build,
+          // Tweak this to match your GitHub project name
+          publicPath: '/webpack-demo/',
           filename: '[name].[chunkhash].js',
           // This is used for require.ensure. The setup
           // will work without but this is useful to set.
